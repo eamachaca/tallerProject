e2576ea3523fc2df61d6350c9af118041319e40e
DeIt0 <eduardito_145@hotmail.com>
2018-06-18 18:29:09 +0000

    Finishing with some error

--- app/FillDistributor.php
+++ app/FillDistributor.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class FillDistributor extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'quantity', 'distributor_id', 'product_id'
+    ];
 }
